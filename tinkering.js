5a5dc85505ab1210c502b57d7d95a56db175f931
Cheever Esler <ames.esler@gmail.com>
2022-09-23 12:25:11 -0300

    more example code

--- tinkering.js
+++ tinkering.js
@@ -6,16 +6,36 @@
 
 // console.log("up up \n \t down down");
 
-let tier = "deck of legends";
-let output = "You'll receive ";
+// let tier = "deck of legends";
+// let output = "You'll receive ";
 
 
-switch (tier) {
-  case "deck of legends":
-    output += "a custom card, ";
-    break;
-  default:
-    output += "and one copy of the Cards Against Humanity card game."  
+// switch (tier) {
+//   case "deck of legends":
+//     output += "a custom card, ";
+//     break;
+//   default:
+//     output += "and one copy of the Cards Against Humanity card game."  
+// }
+
+// console.log(output);
+
+
+// let x = 1;
+// while (x <= 10) {
+//   console.log(x + " mississippi!");
+//   x += 1;
+// }
+
+// for (let i = 0; i < 6; i += 1) {
+//   console.log(i);
+// }
+
+
+for (let xx = 0; xx < 3; xx += 1) {
+  for (let yy = 0; yy < 3; yy += 1) {
+    for (let zz = 0; zz < 3; zz += 1) {
+      console.log(xx + "," + yy + "," + zz);
+    }
+  }   
 }
-
-console.log(output);
