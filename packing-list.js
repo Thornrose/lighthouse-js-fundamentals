c2ab3d7b3f7fcf1b85ede6fcce3f9783f641c76c
Cheever Esler <ames.esler@gmail.com>
2022-09-27 13:03:24 -0300

    packing list exercise completed

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen Stuff to pack:");
-for (let i=0; i < packingList.length; i++) {
+// for (let i=0; i < packingList.length; i++) { // traditional for loop using index variable
+//   console.log(packingList[i]);
+// }
+
+// for (let item of packingList) {              // same as above using for..of
+//   console.log(item);
+// }
+
+let i = 0;                                      // using while, less efficient but still possible
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
